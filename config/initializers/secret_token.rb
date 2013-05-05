60ff6bdcac572849eab3caa2c3ec4dd5096d0368
Numerico <webmaster@numerica.cl>
2013-05-04 21:29:14 -0400

    rails empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ladespensa::Application.config.secret_key_base = '27bf12b64bbaa8a53f802e9050725f01d9d9ad9be7c7a1cb794a2e0d6b796ecd78605146e24f4282f02aa7a81a17ee91470d002dda40de88dccd95cf3c4c57a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ladespensa::Application.config.session_store :encrypted_cookie_store, key: '_ladespensa_session'
